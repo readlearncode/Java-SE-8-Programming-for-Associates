b84e7a6f0be158d1acc56a7143fbe63ad83601ba
Alex Theedom <alex.theedom@gmail.com>
2017-05-12 13:12:07 +0100

    Add examples of class names

---
+++ lesson-1/src/main/java/com/readlearncode/session1/classname/_Ö_.java
@@ -0,0 +1,10 @@
+package com.readlearncode.session1.classname;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class _Ö_ {
+}
